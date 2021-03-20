0084b77182396f1a3fae0444f0790e54e4019a15
Cleyton Vinicius <38632474+vininice@users.noreply.github.com>
2021-03-20 17:54:26 -0300

    optimizing images with next/Image

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image'
+
 import LinkWrapper from 'components/LinkWrapper'
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 
 import * as S from './styles'
+import { useRouter } from 'next/dist/client/router'
 
 type ImageProps = {
   url: string
@@ -13,7 +16,7 @@ export type PlaceTemplateProps = {
   place: {
     slug: string
     name: string
-    description: {
+    description?: {
       html: string
     }
     gallery: ImageProps[]
@@ -21,6 +24,9 @@ export type PlaceTemplateProps = {
 }
 
 export default function PlacesTemplate({ place }: PlaceTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
   return (
     <>
       <LinkWrapper href="/">
@@ -31,12 +37,19 @@ export default function PlacesTemplate({ place }: PlaceTemplateProps) {
           <S.Heading>{place.name}</S.Heading>
 
           <S.Body
-            dangerouslySetInnerHTML={{ __html: place.description.html }}
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
           />
 
           <S.Gallery>
             {place.gallery.map((image, index) => (
-              <img src={image.url} alt={place.name} key={index} />
+              <Image
+                width={1000}
+                height={600}
+                quality={75}
+                src={image.url}
+                alt={place.name}
+                key={index}
+              />
             ))}
           </S.Gallery>
         </S.Container>
