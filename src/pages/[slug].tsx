dfac5102199fa757d0af39f333ea406f4040a687
Cleyton Vinicius <38632474+vininice@users.noreply.github.com>
2021-03-20 14:10:51 -0300

    configured codegen for generating 'types' the queries(graphql)

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,6 @@
-import client from 'grapqhl/client'
-import { GET_PAGES, GET_PAGE_BY_SLUG } from 'grapqhl/querys'
+import client from 'graphql/client'
+import { GetPageBySlugQuery, PagesQuery } from 'graphql/generated/graphql'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
 import { GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import PageTemplate, { PageTemplateProps } from 'templates/Pages'
@@ -15,7 +16,7 @@ export default function Page({ heading, body }: PageTemplateProps) {
 
 //gerando urls da pagina
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<PagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -25,7 +26,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
